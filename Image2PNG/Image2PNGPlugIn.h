f9038918738c9a8fa92c20a99dcb27eb855e352f
Shin'ichiro SUZUKI <shin@szk-engineering.com>
2015-12-09 15:10:39 +0900

    Apple Original Reference Code Version 2009 doesn't work for my environment(Xcode 7.1 on MacOS 10.11). Apple Original Reference Code Version 2009 -> https://developer.apple.com/library/mac/samplecode/ImageExporter/Introduction/Intro.html#//apple_ref/doc/uid/DTS40009329-Intro-DontLinkElementID_2

--- Image2PNG/Image2PNGPlugIn.h
+++ Image2PNG/Image2PNGPlugIn.h
@@ -3,15 +3,16 @@
 //  Image2PNG
 //
 //  Created by Shin'ichiro SUZUKI on 2015/12/09.
-//  Copyright © 2015年 szk-engineering. All rights reserved.
+//  Copyright © 2015 szk-engineering. All rights reserved.
 //
 
 #import <Quartz/Quartz.h>
 
 @interface Image2PNGPlugIn : QCPlugIn
-
-// Declare here the properties to be used as input and output ports for the plug-in e.g.
-//@property double inputFoo;
-//@property (copy) NSString* outputBar;
+{
+  unsigned long _index;
+}
+@property(assign) id<QCPlugInInputImageSource> inputImage;
+@property(assign) NSString* inputPath;
 
 @end
--- Image2PNG/Image2PNGPlugIn.m
+++ Image2PNG/Image2PNGPlugIn.m
@@ -3,7 +3,7 @@
 //  Image2PNG
 //
 //  Created by Shin'ichiro SUZUKI on 2015/12/09.
-//  Copyright © 2015年 szk-engineering. All rights reserved.
+//  Copyright © 2015 szk-engineering. All rights reserved.
 //
 
 // It's highly recommended to use CGL macros instead of changing the current context for plug-ins that perform OpenGL rendering
@@ -12,47 +12,49 @@
 #import "Image2PNGPlugIn.h"
 
 #define	kQCPlugIn_Name				@"Image2PNG"
-#define	kQCPlugIn_Description		@"Image2PNG description"
+#define	kQCPlugIn_Description		@"Export Image as Portable Network Graphics(PNG)"
 
 @implementation Image2PNGPlugIn
 
-// Here you need to declare the input / output properties as dynamic as Quartz Composer will handle their implementation
-//@dynamic inputFoo, outputBar;
+@dynamic inputImage, inputPath;
 
 + (NSDictionary *)attributes
 {
-	// Return a dictionary of attributes describing the plug-in (QCPlugInAttributeNameKey, QCPlugInAttributeDescriptionKey...).
-    return @{QCPlugInAttributeNameKey:kQCPlugIn_Name, QCPlugInAttributeDescriptionKey:kQCPlugIn_Description};
+  // Return a dictionary of attributes describing the plug-in (QCPlugInAttributeNameKey, QCPlugInAttributeDescriptionKey...).
+  return @{QCPlugInAttributeNameKey:kQCPlugIn_Name, QCPlugInAttributeDescriptionKey:kQCPlugIn_Description};
 }
 
 + (NSDictionary *)attributesForPropertyPortWithKey:(NSString *)key
 {
-	// Specify the optional attributes for property based ports (QCPortAttributeNameKey, QCPortAttributeDefaultValueKey...).
-	return nil;
+  if ([key isEqualToString:@"inputImage"]) {
+    return [NSDictionary dictionaryWithObjectsAndKeys:@"Image", QCPortAttributeNameKey, nil];
+  }
+  if ([key isEqualToString:@"inputPath"]) {
+    return [NSDictionary dictionaryWithObjectsAndKeys:@"Destination Path", QCPortAttributeNameKey, @"~/Desktop", QCPortAttributeDefaultValueKey, nil];
+  }
+  return nil;
 }
 
 + (QCPlugInExecutionMode)executionMode
 {
-	// Return the execution mode of the plug-in: kQCPlugInExecutionModeProvider, kQCPlugInExecutionModeProcessor, or kQCPlugInExecutionModeConsumer.
-	return kQCPlugInExecutionModeProcessor;
+  // Return the execution mode of the plug-in: kQCPlugInExecutionModeProvider, kQCPlugInExecutionModeProcessor, or kQCPlugInExecutionModeConsumer.
+  return kQCPlugInExecutionModeConsumer;
 }
 
 + (QCPlugInTimeMode)timeMode
 {
-	// Return the time dependency mode of the plug-in: kQCPlugInTimeModeNone, kQCPlugInTimeModeIdle or kQCPlugInTimeModeTimeBase.
-	return kQCPlugInTimeModeNone;
+  // Return the time dependency mode of the plug-in: kQCPlugInTimeModeNone, kQCPlugInTimeModeIdle or kQCPlugInTimeModeTimeBase.
+  return kQCPlugInTimeModeNone;
 }
 
 - (instancetype)init
 {
-	self = [super init];
-	if (self) {
-		// Allocate any permanent resource required by the plug-in.
-	}
-	
-	return self;
+  self = [super init];
+  if (self) {
+    //
+  }
+  return self;
 }
-
 
 @end
 
@@ -60,39 +62,92 @@
 
 - (BOOL)startExecution:(id <QCPlugInContext>)context
 {
-	// Called by Quartz Composer when rendering of the composition starts: perform any required setup for the plug-in.
-	// Return NO in case of fatal failure (this will prevent rendering of the composition to start).
-	
-	return YES;
+  // Called by Quartz Composer when rendering of the composition starts: perform any required setup for the plug-in.
+  // Return NO in case of fatal failure (this will prevent rendering of the composition to start).
+  _index = 0;
+  return YES;
 }
 
 - (void)enableExecution:(id <QCPlugInContext>)context
 {
-	// Called by Quartz Composer when the plug-in instance starts being used by Quartz Composer.
+  // Called by Quartz Composer when the plug-in instance starts being used by Quartz Composer.
 }
 
 - (BOOL)execute:(id <QCPlugInContext>)context atTime:(NSTimeInterval)time withArguments:(NSDictionary *)arguments
 {
-	/*
-	Called by Quartz Composer whenever the plug-in instance needs to execute.
-	Only read from the plug-in inputs and produce a result (by writing to the plug-in outputs or rendering to the destination OpenGL context) within that method and nowhere else.
-	Return NO in case of failure during the execution (this will prevent rendering of the current frame to complete).
-	
-	The OpenGL context for rendering can be accessed and defined for CGL macros using:
-	CGLContextObj cgl_ctx = [context CGLContextObj];
-	*/
-	
-	return YES;
+  /*
+   Called by Quartz Composer whenever the plug-in instance needs to execute.
+   Only read from the plug-in inputs and produce a result (by writing to the plug-in outputs or rendering to the destination OpenGL context) within that method and nowhere else.
+   Return NO in case of failure during the execution (this will prevent rendering of the current frame to complete).
+   
+   The OpenGL context for rendering can be accessed and defined for CGL macros using:
+   CGLContextObj cgl_ctx = [context CGLContextObj];
+   */
+  id<QCPlugInInputImageSource> qcImage = self.inputImage;
+  NSString* pixelFormat;
+  CGColorSpaceRef colorSpace;
+  CGDataProviderRef dataProvider;
+  CGImageRef cgImage;
+  CGImageDestinationRef imageDestination;
+  NSURL* fileURL;
+  BOOL success;
+  
+  if (![self didValueForInputKeyChange:@"inputImage"] || !qcImage || ![self.inputPath length]) {
+    return YES;
+  }
+  
+  colorSpace = [qcImage imageColorSpace];
+  if(CGColorSpaceGetModel(colorSpace) == kCGColorSpaceModelMonochrome) {
+    pixelFormat = QCPlugInPixelFormatI8;
+  }
+  else if(CGColorSpaceGetModel(colorSpace) == kCGColorSpaceModelRGB) {
+#if __BIG_ENDIAN__
+    pixelFormat = QCPlugInPixelFormatARGB8;
+#else
+    pixelFormat = QCPlugInPixelFormatBGRA8;
+#endif
+  } else {
+    return NO;
+  }
+  
+  if (![qcImage lockBufferRepresentationWithPixelFormat:pixelFormat colorSpace:colorSpace forBounds:[qcImage imageBounds]]) {
+    return NO;
+  }
+  
+  dataProvider = CGDataProviderCreateWithData(NULL, [qcImage bufferBaseAddress], [qcImage bufferPixelsHigh] * [qcImage bufferBytesPerRow], NULL);
+  cgImage = CGImageCreate([qcImage bufferPixelsWide], [qcImage bufferPixelsHigh], 8, (pixelFormat == QCPlugInPixelFormatI8 ? 8 : 32), [qcImage bufferBytesPerRow], colorSpace, (pixelFormat == QCPlugInPixelFormatI8 ? 0 : kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host), dataProvider, NULL, false, kCGRenderingIntentDefault);
+  CGDataProviderRelease(dataProvider);
+  if(cgImage == NULL) {
+    [qcImage unlockBufferRepresentation];
+    return NO;
+  }
+  NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+  [formatter setDateFormat:@"yyyyMMdd-HHmmss"];
+  fileURL = [NSURL fileURLWithPath:[[self.inputPath stringByStandardizingPath] stringByAppendingPathComponent:[NSString stringWithFormat:@"%@_%05lu.png", [formatter stringFromDate:[NSDate date]], ++_index]]];
+  imageDestination = (fileURL ? CGImageDestinationCreateWithURL((CFURLRef)fileURL, kUTTypePNG, 1, NULL) : NULL);
+  if(imageDestination == NULL) {
+    CGImageRelease(cgImage);
+    [qcImage unlockBufferRepresentation];
+    return NO;
+  }
+  CGImageDestinationAddImage(imageDestination, cgImage, NULL);
+  success = CGImageDestinationFinalize(imageDestination);
+  CFRelease(imageDestination);
+  
+  CGImageRelease(cgImage);
+  [qcImage unlockBufferRepresentation];
+  
+  return success;
 }
 
 - (void)disableExecution:(id <QCPlugInContext>)context
 {
-	// Called by Quartz Composer when the plug-in instance stops being used by Quartz Composer.
+  // Called by Quartz Composer when the plug-in instance stops being used by Quartz Composer.
 }
 
 - (void)stopExecution:(id <QCPlugInContext>)context
 {
-	// Called by Quartz Composer when rendering of the composition stops: perform any required cleanup for the plug-in.
+  // Called by Quartz Composer when rendering of the composition stops: perform any required cleanup for the plug-in.
 }
 
 @end
