0adcf1b1e577a945204c0ca435962900e0ccca51
Shin'ichiro SUZUKI <shin@szk-engineering.com>
2015-12-09 12:32:47 +0900

    Initial Commit

---
+++ Image2PNG/Image2PNGPlugIn.h
@@ -0,0 +1,17 @@
+//
+//  Image2PNGPlugIn.h
+//  Image2PNG
+//
+//  Created by Shin'ichiro SUZUKI on 2015/12/09.
+//  Copyright © 2015年 szk-engineering. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface Image2PNGPlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
